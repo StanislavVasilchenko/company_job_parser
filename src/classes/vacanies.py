546f3f4907012d0f281e4b73687f5d48bf71ba56
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-10-14 12:58:05 +0300

    Реализация класса VacanciesFromEmployers

--- src/classes/vacanies.py
+++ src/classes/vacanies.py
@@ -1,19 +1,62 @@
 import requests
-from src.classes.employer import EmployersHH
+
+
+class VacanciesFromEmployers:
+
+    def __init__(self, employers: list):
+        self.employers = employers
+        self.params = {
+            "page": 0,
+            "per_page": 100,
+            "text": "Python",
+            "area": 1,
+            "professional_role": '96'
+
+        }
+        self.all_vac = self.creation_of_vacancy_objects()
+
+    def get_all_vacancies_from_employers(self) -> list[dict]:
+        vac_employers = []
+        for emp in self.employers:
+            url = emp.vacancies_url
+            pages = requests.get(url=url, params=self.params).json().get("page")
+            for page in range(pages + 1):
+                self.params["page"] = page
+                response_vac = requests.get(url=url, params=self.params).json()
+                vac_employers.extend(response_vac["items"])
+            print(f"{emp.employer_name} - done")
+        return vac_employers
+
+    def get_format_vacancies_from_employers(self) -> list[dict]:
+        vacancies = self.get_all_vacancies_from_employers()
+        format_vacancies = []
+        for vac in vacancies:
+            format_vac = {
+                "employer_id": int(vac["employer"]["id"]),
+                "vacancy_name": vac["name"],
+                "city": vac["area"]["name"],
+                "url": vac["alternate_url"],
+                "requirement": vac["snippet"]["requirement"],
+                "responsibility": vac["snippet"]["responsibility"],
+                "schedule": vac["schedule"]["name"]
+            }
+            format_vacancies.append(format_vac)
+        return format_vacancies
+
+    def creation_of_vacancy_objects(self) -> list[object]:
+        vacancies_for_packaging = self.get_format_vacancies_from_employers()
+        creation_of_vacancy = [Vacancy(**format_vac) for format_vac in vacancies_for_packaging]
+        return creation_of_vacancy
 
 
 class Vacancy:
 
     def __init__(self, employer_id: int, vacancy_name: str, city: str,
-                 salary_from: int, salary_to: int,
-                 currency: str, url: str, requirement: str,
+                 url: str, requirement: str,
                  responsibility: str, schedule: str):
         self.employer_id = employer_id
         self.vacancy_name = vacancy_name
         self.city = city
-        self.salary_from = salary_from
-        self.salary_to = salary_to
-        self.currency = currency
         self.url = url
         self.requirement = requirement
         self.responsibility = responsibility
@@ -22,5 +65,4 @@ class Vacancy:
     def __repr__(self):
         return (f"Name - {self.vacancy_name}\n"
                 f"City - {self.city}\n"
-                f"salary_from - {self.salary_from}, salary_to - {self.salary_to}\n"
-                f"URL - {self.url}")
+                f"URL - {self.url}\n")
