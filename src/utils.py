1fd24b87e7b4b83077bb4c843358ee24e600beba
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-10-15 14:44:23 +0300

    Добавлена функция filling_out_the_vacancy_table для заполнения таблицы vacancy

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,6 @@ import psycopg2
 
 
 def create_database(params, db_name):
-    # params = config()
     conn = psycopg2.connect(dbname="postgres", **params)
     conn.autocommit = True
     with conn.cursor() as cur:
@@ -15,17 +14,17 @@ def create_database(params, db_name):
 def create_tables(cur, params, db_name):
     cur.execute("""CREATE TABLE employers(
                     employer_id int,
-                    employer_name varchar(50),
+                    employer_name varchar(100),
                     vacancies_url text
                     )
                 """)
     cur.execute("""CREATE TABLE vacancy (
-                    vacancy_id SERIAL,
-                    employer_id smallint,
+                    vacancy_id SERIAL ,
+                    employer_id int,
                     vacancy_name varchar(100),
-                    salary_from smallint,
-                    salary_to smallint,
-                    city varchar(30),
+                    salary_from int,
+                    salary_to int,
+                    city varchar(100),
                     url text,
                     requirement text,
                     responsibility text,
@@ -40,3 +39,18 @@ def filling_out_the_employer_table(cur, employers_list: list[object]) -> None:
                      employer.vacancies_url)
                     )
 
+def filling_out_the_vacancy_table(cur, vacancyes_list: list[object]) -> None:
+    for vacancy in vacancyes_list:
+        cur.execute("""INSERT INTO vacancy (employer_id, vacancy_name, salary_from,
+                                            salary_to, city, url, requirement, responsibility,
+                                            schedule) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)""",
+                    (vacancy.employer_id,
+                     vacancy.vacancy_name,
+                     vacancy.salary_from,
+                     vacancy.salary_to,
+                     vacancy.city,
+                     vacancy.url,
+                     vacancy.requirement,
+                     vacancy.responsibility,
+                     vacancy.schedule)
+                    )
