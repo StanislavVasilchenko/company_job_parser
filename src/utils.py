89c93d81aff912415c13fe125cac67f584846efa
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-10-15 12:34:34 +0300

    Добавлена функция create_tables для создания таблиц в БД

--- src/utils.py
+++ src/utils.py
@@ -2,11 +2,32 @@ from config import config
 import psycopg2
 
 
-def create_database(db_name="employers_vacancies"):
-    params = config()
+def create_database(params, db_name):
+    # params = config()
     conn = psycopg2.connect(dbname="postgres", **params)
     conn.autocommit = True
     with conn.cursor() as cur:
         cur.execute(f"DROP DATABASE IF EXISTS {db_name}")
         cur.execute(f"CREATE DATABASE {db_name}")
     conn.close
+
+
+def create_tables(cur, params, db_name):
+    cur.execute("""CREATE TABLE employers(
+                    employer_id smallint,
+                    employer_name varchar(50),
+                    vacancies_url text
+                    )
+                """)
+    cur.execute("""CREATE TABLE vacancy (
+                    vacancy_id SERIAL,
+                    employer_id smallint,
+                    vacancy_name varchar(100),
+                    salary_from smallint,
+                    salary_to smallint,
+                    city varchar(30),
+                    url text,
+                    requirement text,
+                    responsibility text,
+                    schedule text)
+        """)
