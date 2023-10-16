a877119f3cdd864c77f8b94bb4b8c415a706c601
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-10-16 21:13:55 +0300

    Добавлена функция get_all_vacancies в класс DBManager

--- src/classes/db_manager.py
+++ src/classes/db_manager.py
@@ -11,21 +11,23 @@ class DBManager:
     def get_companies_and_vacancies_count(self):
         """Получает список всех компаний и количество вакансий у каждой компании.
         И записывает полученные данные в csv файл companies_and_vacancies_count.csv"""
-
         rows_name = ["Название компании", "Кол-во вакансий"]
         file_name = "csv_files/companies_and_vacancies_count.csv"
-
         self.cur.execute("""select employer_name, count(vacancy_id) as number_of_vacancies from vacancy
                                 join employers using (employer_id)
                                 group by employer_name""")
 
         write_in_csv(file_name, self.cur.fetchall(), rows_name)
-        self.con.close()
 
     def get_all_vacancies(self):
         """Получает список всех вакансий с указанием названия компании,
          названия вакансии и зарплаты и ссылки на вакансию."""
-        pass
+        rows_name = ["Название компании", "Вакансия", "ЗП от", "ЗП до", "Ссылка на вакансию"]
+        file_name = "csv_files/all_vacancies.csv"
+
+        self.cur.execute("""select employer_name, vacancy_name, salary_from, salary_to, url from vacancy
+                            join employers using(employer_id)""")
+        write_in_csv(file_name, self.cur.fetchall(), rows_name)
 
     def get_avg_salary(self):
         """Получает среднюю зарплату по вакансиям."""
