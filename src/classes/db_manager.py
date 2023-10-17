c1e97a7f288b105eedcd6734360dbce76a4a7dbe
Stanislav Vasilchenko <stanilav.vasilchenko@yandex.ru>
2023-10-17 21:13:22 +0300

    функции возвращают кортеж

--- src/classes/db_manager.py
+++ src/classes/db_manager.py
@@ -1,5 +1,4 @@
 import psycopg2
-from src.utils import write_in_csv
 
 
 class DBManager:
@@ -8,70 +7,66 @@ class DBManager:
         self.con = psycopg2.connect(**self.params)
         self.cur = self.con.cursor()
 
-    def get_companies_and_vacancies_count(self):
+    def get_companies_and_vacancies_count(self) -> tuple:
         """Получает список всех компаний и количество вакансий у каждой компании.
         И записывает полученные данные в csv файл companies_and_vacancies_count.csv"""
 
         rows_name = ["Название компании", "Кол-во вакансий"]
-        file_name = "csv_files/companies_and_vacancies_count.csv"
 
         self.cur.execute("""select employer_name, count(vacancy_id) as number_of_vacancies from vacancy
                                 join employers using (employer_id)
                                 group by employer_name""")
 
         data = self.cur.fetchall()
-        write_in_csv(file_name, data, rows_name)
+        return f"csv_files/{self.get_companies_and_vacancies_count.__name__}.csv", data, rows_name
 
-    def get_all_vacancies(self):
+    def get_all_vacancies(self) -> tuple:
         """Получает список всех вакансий с указанием названия компании,
          названия вакансии и зарплаты и ссылки на вакансию. И записывает данные в
          csv файл all_vacancies.csv"""
 
         rows_name = ["Название компании", "Вакансия", "ЗП от", "ЗП до", "Ссылка на вакансию"]
-        file_name = "csv_files/all_vacancies.csv"
 
         self.cur.execute("""select employer_name, vacancy_name, salary_from, salary_to, url from vacancy
                             join employers using(employer_id)""")
         data = self.cur.fetchall()
-        write_in_csv(file_name, data, rows_name)
+        return f"csv_files/{self.get_all_vacancies.__name__}.csv", data, rows_name
 
-    def get_avg_salary(self):
+    def get_avg_salary(self) -> tuple:
         """Получает среднюю зарплату по вакансиям. И сохраняет результат в csv файл avg_salary.csv"""
 
         rows_name = ["Средняя зарплата от", "Средняя зарплата до"]
-        file_name = "csv_files/avg_salary.csv"
 
         self.cur.execute("""select avg(salary_from), avg(salary_to) from vacancy""")
         data = self.cur.fetchall()
-        write_in_csv(file_name, data, rows_name)
+        return f"csv_files/{self.get_avg_salary.__name__}.csv", data, rows_name
 
-    def get_vacancies_with_higher_salary(self):
+    def get_vacancies_with_higher_salary(self) -> tuple:
         """Получает список всех вакансий,
          у которых зарплата выше средней по всем вакансиям."""
 
         rows_name = ["Название вакансии", "Зарплата от", "Зарплата до", "Ссылка на вакансию"]
-        file_name = "csv_files/vacancies_with_higher_salary.csv"
 
         self.cur.execute("""select vacancy_name, salary_from, salary_to, url from vacancy
                             where salary_from > (select avg(salary_from) from vacancy)
                             and salary_to > (select avg(salary_to) from vacancy)""")
         data = self.cur.fetchall()
-        write_in_csv(file_name, data, rows_name)
+        return f"csv_files/{self.get_vacancies_with_higher_salary.__name__}.csv", data, rows_name
 
-    def get_vacancies_with_keyword(self, key_words: list[str]):
+    def get_vacancies_with_keyword(self, key_words: list[str]) -> tuple:
         """Получает список всех вакансий,
          в названии которых содержатся переданные в метод слова, например python."""
 
-        rows_name = ["Название вакансии", "Зарплата от", "Зарплата до", "Ссылка на вакансию"]
-        file_name = "csv_files/vacancies_with_keyword.csv"
+        rows_name = ["Название вакансии", "Город", "Требования к соискателю", "Зарплата от", "Зарплата до",
+                     "Ссылка на вакансию"]
         data = []
         for word in key_words:
-            self.cur.execute(f"""select vacancy_name, city, requirement, salary_from, salary_to, url from vacancy
+            self.cur.execute(f"""select vacancy_name, city, requirement, salary_from, salary_to, url
+                                from vacancy
                                 where requirement like '%{word}'
                                 or requirement like '%{word}%'
                                 or requirement like '{word}%'
                                 """)
             data_request_from_db = self.cur.fetchall()
             data.extend(data_request_from_db)
-        write_in_csv(file_name, data, rows_name)
-
+        return f"csv_files/{self.get_vacancies_with_keyword.__name__}.csv", data, rows_name
